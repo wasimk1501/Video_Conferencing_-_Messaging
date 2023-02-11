a56e9dba58f173af7535689570ceef637d9357fc
Wasim Khan <wasimk1501@gmail.com>
2023-02-11 22:46:02 +0530

    third commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Column(
+        children: [
+          Container(child: Text("Create a new meeting.")),
+          Container(child: Text("Enter the code")),
+        ],
+      ),
+    );
+  }
+}
